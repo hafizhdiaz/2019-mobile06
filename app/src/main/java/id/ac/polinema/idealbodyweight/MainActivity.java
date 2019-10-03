22d445ecde657fc6d9e1d2a320aba487483236ae
hafizhdiaz <hafizhdiaz@gmail.com>
2019-10-03 14:29:59 +0700

    Komunikasi Fragment Bundle

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import id.ac.polinema.idealbodyweight.Fragment.AboutFragment;
+
 public class MainActivity extends AppCompatActivity {
 
 	// Deklarasikan atribut Fragment di sini
+	AboutFragment blankFragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		blankFragment = AboutFragment.newInstance("Hafizh Dias");
 	}
 
 	@Override
@@ -26,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
+		if(item.getItemId() == R.id.menu_about){
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container, blankFragment)
+					.addToBackStack(null)
+					.commit();
+		}
+
 
 		return super.onOptionsItemSelected(item);
 	}
